86ee006d8f1a083175cdfc34a8107149d1afd0eb
BenilDavid <DoodleDavid>
2023-01-12 00:35:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ function App() {
   // timer function
   const setTimerFunction = () => {
     // Set the date we're counting down to
-    var countDownDate = new Date("Jan 13, 2023 00:45:00").getTime();
+    var countDownDate = new Date("Jan 13, 2023 00:40:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
